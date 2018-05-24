bc19db5d4c957a9423e1afbc349a5badc9bfe5d8
Hayden Hughes <haydenhughes@protonmail.com>
2018-05-24 20:12:13 +1000

    Add doc string

--- point.py
+++ point.py
@@ -2,4 +2,14 @@ from collections import namedtuple
 
 
 class Point(namedtuple('Point', ['x', 'y', 'z'])):
+    """A named tuple for referencing a point on a plane.
+
+    :Parameters:
+        `x` : int
+            x coordinate
+        `y` : int
+            y coordinate
+        `z` : int
+            z coordinate
+    """
     pass
