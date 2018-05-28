b7a97db3b2a7125f6f397c525c659ae52097aad0
Hayden Hughes <haydenhughes@protonmail.com>
2018-05-28 18:52:16 +1000

    Update tests

--- test/test_point.py
+++ test/test_point.py
@@ -4,16 +4,8 @@ from engine.point import Point
 
 class TestPoint(unittest.TestCase):
     def setUp(self):
-        self.point = Point(1, 2, 3)
+        self.point1 = Point(-2, 5)
+        self.point2 = Point(4, -3)
 
-    def test_x(self):
-        self.assertEqual(self.point.x, 1)
-        self.assertEqual(self.point[0], 1)
-
-    def test_y(self):
-        self.assertEqual(self.point.y, 2)
-        self.assertEqual(self.point[1], 2)
-
-    def test_z(self):
-        self.assertEqual(self.point.z, 3)
-        self.assertEqual(self.point[2], 3)
+    def test_distance(self):
+        self.assertEqual(self.point1.distance(self.point2), 10)
