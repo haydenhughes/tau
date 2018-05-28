f65a3c573b14f0993cb5c1802bb172368e710f67
Hayden Hughes <haydenhughes@protonmail.com>
2018-05-28 18:43:39 +1000

    Rework point

--- engine/point.py
+++ engine/point.py
@@ -1,12 +1,39 @@
-from collections import namedtuple
+import math
 
 
-class Point(namedtuple('Point', ['x', 'y', 'z'])):
-    """A named tuple for referencing a point on a plane.
+class Point:
+    """References a point on a plane.
 
     Attributes:
         x: An integer for the x coordinate.
         y: An integer for the y coordinate.
         z: An integer for the z coordinate, used for referencing a layer.
     """
-    pass
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def angle(self, other):
+        """Get the angle between two points in 2D.
+
+        Args:
+            other: Another point.
+
+        Returns:
+            A float in degrees of the angle between self and other points.
+        """
+        return math.degrees(math.atan2(other.y, other.x)
+                            - math.atan2(self.y, self.x))
+
+    def distance(self, other):
+        """Calculate distance between self and another object.
+
+        Args:
+            other: Another sprite.
+
+        Returns:
+            A float of the calculated distance in pixels.
+        """
+        return math.hypot(other.x - self.x, other.y - self.y)
