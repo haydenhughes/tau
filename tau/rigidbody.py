79b33262b2abea97d7314a7d56f7d12c8907db5c
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-10-01 18:12:03 +1000

    Remove the requirement for passing app to Rigidbodies

--- tau/rigidbody.py
+++ tau/rigidbody.py
@@ -3,19 +3,17 @@ from tau.vector import Vector
 
 
 class Rigidbody(object):
-    def __init__(self, app,
+    def __init__(self,
                  x: int,
                  y: int,
                  color=(255, 255, 255),
                  velocity: Vector = Vector(),
                  mass=0):
-        app.push_handlers(self)  # add to event stack to register on_draw
         self.x = x
         self.y = y
         self.color = color
         self.velocity = velocity
         self.mass = mass
-        pyglet.clock.schedule_interval(self.update, 1/60)
 
     def move(self):
         self.x += self.velocity.x
--- tau/tau.py
+++ tau/tau.py
@@ -2,10 +2,31 @@ import pyglet
 
 
 class Tau(pyglet.window.Window):
-    """ Helper to set up a window and manage object drawing."""
+    """ Helper to set up a window and manage objects.
+    Attributes:
+        framerate: A float of the amount of time in seconds each frame is
+            displayed.
+    """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, framerate=1/60, **kwargs):
         super().__init__(*args, **kwargs)
+        self.framerate = framerate
+        self._objects = []
+
+    def add_objects(self, *object):
+        self._objects.extend(object)
+
+    def __len__(self):
+        return len(self._objects)
+
+    def __getitems__(self, index):
+        if index < len(self):
+            return self._objects[index]
+        raise IndexError('Object out of range')
 
     def run(self):
+        for object in self._objects:
+            self.push_handlers(object)
+            pyglet.clock.schedule_interval(object.update, self.framerate)
+
         pyglet.app.run()
