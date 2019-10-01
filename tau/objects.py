578050783b9a4eec39b6c4f554da8b3a18c26a32
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-10-01 18:51:09 +1000

    Change rigidbody to objects

--- tau/objects.py
+++ tau/objects.py
@@ -3,17 +3,28 @@ from tau.vector import Vector
 
 
 class PointMass(object):
+    """The most basic object.
+    Attributes:
+        x: A int of the x coordinate.
+        y: A int of the y coordinate.
+        mass: A float of the mass of the point mass
+        velocity: A Vector of the current velocity of the point.
+        color: A tuple of RGB values to color the point.
+            Default: (255, 255, 255)
+    """
+
     def __init__(self,
                  x: int,
                  y: int,
-                 color=(255, 255, 255),
+                 mass: float = 0,
                  velocity: Vector = Vector(),
-                 mass=0):
+                 color=(255, 255, 255)):
         self.x = x
         self.y = y
         self.color = color
         self.velocity = velocity
         self.mass = mass
+        self.app = None
 
     def update(self, dt):
         self.x += self.velocity.x
@@ -23,3 +34,4 @@ class PointMass(object):
         pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
                              ('v2i', (self.x, self.y)),
                              ('c3B', self.color))
+
