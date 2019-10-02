b5f3eb1502e5ce52b37511f714a0e6c7b18219bb
Hayden Hughes <hayden@vulpes.chickenkiller.com>
2019-10-02 11:51:57 +1000

    Improve documentaion for PointMass

--- tau/objects.py
+++ tau/objects.py
@@ -5,12 +5,14 @@ from tau.vector import Vector
 
 class PointMass(object):
     """The most basic object.
+
     Attributes:
-        x: A int of the x coordinate.
-        y: A int of the y coordinate.
-        mass: A float of the mass of the point mass
-        velocity: A Vector of the current velocity of the point.
-        color: A tuple of RGB values to color the point.
+        x: A int of the current x coordinate.
+        y: A int of the current y coordinate.
+        mass: A float of the mass of the object.
+        velocity: A Vector of the current velocity of the object.
+        acceleration: A Vector of the current acceleration of the object.
+        color: A tuple of RGB values to color the object.
             Default: (255, 255, 255)
     """
     GRAVITATIONAL_CONSTANT = 6.673e-11
@@ -30,8 +32,7 @@ class PointMass(object):
         self.mass = mass
         self.app = None
 
-    def apply_acceleration(self, dt):
-
+    def _apply_acceleration(self, dt):
         # Account for gravity between objects
         for object in self.app:
             if object != self:
@@ -39,13 +40,19 @@ class PointMass(object):
 
         self.velocity += self.acceleration * dt
 
-    def move(self):
+    def _move(self):
         self.x += self.velocity.x
         self.y += self.velocity.y
 
     def update(self, dt):
-        self.apply_acceleration(dt)
-        self.move()
+        """Called every frame flip by Tau
+
+        Args:
+            dt: The floored (rounded down) amount of whole seconds that has
+                elasped between frame refreshes. This value is generally 0.
+        """
+        self._apply_acceleration(dt)
+        self._move()
 
     def on_draw(self):
         pyglet.graphics.draw(1, pyglet.gl.GL_POINTS,
@@ -53,12 +60,37 @@ class PointMass(object):
                              ('c3B', self.color))
 
     def distance_to(self, other: 'PointMass'):
+        """Calcualate the distance between objects.
+
+        Args:
+            other: Another PointMass object.
+
+        Returns:
+            A float of the amount of pixels between self and other.
+        """
         return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
 
     def angle_between(self, other: 'PointMass'):
+        """Calcualate the angle between objects.
+
+        Args:
+            other: Another PointMass object.
+
+        Returns:
+            A float of the angle in radians from the normal (positive y axis)
+            to other from self.
+        """
         return math.atan2(other.x - self.x, other.y - self.y)
 
     def gravitational_force(self, other: 'PointMass'):
+        """Calcualate the gravitational force between objects.
+
+        Args:
+            other: Another PointMass object.
+
+        Returns:
+            A float of the force in Newtons of gravity between self and other.
+        """
         gf = (self.GRAVITATIONAL_CONSTANT * self.mass
               * other.mass) / (self.distance_to(other) ** 2)
         return Vector(gf * math.sin(self.angle_between(other)),
